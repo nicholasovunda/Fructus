17879c7145456e57de339cebba10e93bb7254acb
nicholas <nicholasovunda@gmail.com>
2022-06-12 18:59:43 +0100

    implemented the fruit details page containing information about each fruit

---
+++ Fructus/View/SourceLinkVIew.swift
@@ -0,0 +1,30 @@
+//
+//  SourceLinkVIew.swift
+//  Fructus
+//
+//  Created by Nicholas on 12/06/2022.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        GroupBox() {
+            HStack {
+                Text("Content Source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+struct SourceLinkVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
